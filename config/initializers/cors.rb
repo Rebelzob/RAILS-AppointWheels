7bbc41ea7d1afd56800f27ef078ecb584fd2de47
Rebelzob <karimdavid07@gmail.com>
2024-02-16 13:26:12 +0000

    Update CORS origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://appointwheels.onrender.com', 'https://appoint-wheels.onrender.com'
+    origins 'https://react-appointwheels.fly.dev'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head], 
